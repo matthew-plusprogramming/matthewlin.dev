2edc329bbfa8b848774224cbf14230c978b38a2d
battletag13 <matthewlinplusprogramming@gmail.com>
2020-06-19 20:01:54 -0700

    Base React App + Navbar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './materialize.scss';
+import './styles/defaults.scss';
+import './styles/app.scss';
+import './styles/nav.scss';
+
+// on component did mount m init
+import M from 'materialize-css';
+
+const App = () => (
+  <>
+    <header>
+      <nav>
+        <ul>
+          <li id="brand-logo">Matthew Lin</li>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Testimonials</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  </>
+);
+export default App;
