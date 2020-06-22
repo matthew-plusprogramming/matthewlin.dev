3b2bd3046a2e4b300693b013cb12949393b28575
battletag13 <matthewlinplusprogramming@gmail.com>
2020-06-21 19:44:16 -0700

    Implemented always learning section

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -39,6 +39,26 @@ const Portfolio = () => {
             <img id="hero" src={hero} alt="" />
           </div>
         </div>
+        <div id="always-learning-section" className="invert-colors">
+          <div className="row center">
+            <h4 className="large-bottom-margin">Always Learning!</h4>
+          </div>
+          <div className="row center">
+            <p className="flow-text">
+              Since beginning my journey as a budding programmer almost four
+              years ago, I've explored a wide range of computer science related
+              fields, studied many of them, and worked on various personal and
+              freelance programming projects. In addition to theoretical and
+              personal practical knowledge, I also have two summers of
+              professional work experience as a software engineer.
+            </p>
+          </div>
+        </div>
+        <div id="companies-section">
+          <div className="row center">
+            <h4 className="large-bottom-margin">Companies I've Worked With</h4>
+          </div>
+        </div>
       </main>
     </>
   );
