0f6eeac133783046893f706ab3140a4b35c62e8f
battletag13 <matthewlinplusprogramming@gmail.com>
2020-06-22 13:51:11 -0700

    Added pages section to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from 'react-router-dom';
+
+import {NavigationContext} from '../contexts/NavigationContext';
 
 import '../styles/footer.scss';
 
 import {routes} from './constants';
 
 const Footer = () => {
+  const navContext = useContext(NavigationContext);
+
   return (
     <footer>
       <div className="flex-row">
@@ -17,6 +21,59 @@ const Footer = () => {
             matthewlinplusprogramming@gmail.com
           </a>
         </h5>
+      </div>
+      <div className="flex-row">
+        <div id="footer-left">
+          <div className="flex-row">
+            <div className="flex-col">
+              <h5 className="light-text">Pages</h5>
+
+              <h6>
+                <Link
+                  to={routes.about}
+                  onClick={() =>
+                    navContext.updateNavLocation(navContext.aboutLinkRef)
+                  }>
+                  About
+                </Link>
+              </h6>
+              <h6>
+                <Link
+                  to={routes.portfolio}
+                  onClick={() =>
+                    navContext.updateNavLocation(navContext.portfolioLinkRef)
+                  }>
+                  Portfolio
+                </Link>
+              </h6>
+              <h6>
+                <Link
+                  to={routes.testimonials}
+                  onClick={() =>
+                    navContext.updateNavLocation(navContext.testimonialsLinkRef)
+                  }>
+                  Testimonials
+                </Link>
+              </h6>
+              <h6>
+                <Link
+                  to={routes.contact}
+                  onClick={() =>
+                    navContext.updateNavLocation(navContext.contactLinkRef)
+                  }>
+                  Contact
+                </Link>
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div id="footer-right">
+          <div className="flex-row">
+            <div className="flex-col">
+              <h5 className="light-text">Find me Online</h5>
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
