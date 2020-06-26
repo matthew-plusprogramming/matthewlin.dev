3354454619a0a49a994a7ad893acd89bd759838f
battletag13 <matthewlinplusprogramming@gmail.com>
2020-06-26 13:45:46 -0700

    Implemented About page descriptions + Bugfixes

--- src/components/About/ImageSection.js
+++ src/components/About/ImageSection.js
@@ -6,12 +6,15 @@ const ImageSection = (props) => {
       <div className="flex-row center image-section-title">
         <h5 className="medium-text">{props.title}</h5>
       </div>
-      <div className="flex-row center">
+      <div className="flex-row center bottom-margin">
         <div className="image-section-wrapper">
           {props.images.map((image, index) => (
             <img src={image} key={index} alt="" />
           ))}
         </div>
+      </div>
+      <div className="flex-row center image-section-description">
+        <p>{props.children}</p>
       </div>
     </div>
   );
