46847bb7ebde23dd45f43cd725d593fde698aa41
battletag13 <matthewlinplusprogramming@gmail.com>
2020-06-26 14:11:53 -0700

    Added about page descriptions + styling improvements

--- src/About.js
+++ src/About.js
@@ -33,69 +33,108 @@ const About = () => {
               title="My current (and future) schools"
               images={[mbhs, sdccd, berkeley]}>
               <span>
-                <bold is="custom">MBHS</bold> <br />I am currently a Junior at
-                Mission Bay High School. I currently take part in many
-                extracurriculars that I love doing (most listed to the right). I
-                am a co-founder of the Mission Bay High School Entrepreneurship
-                Club and academically, I currently hold a{' '}
-                <emphasize is="custom">cumulative 4.84 GPA.</emphasize>
-              </span>
-              <span>
-                <bold is="custom">SDCCD</bold> <br />I am currently a student in
-                the San Diego Community College District pursuing an{' '}
-                <emphasize is="custom">
-                  Associate Degree of Science in Computer Science.
-                </emphasize>{' '}
-                I currently hold a <emphasize is="custom">4.0 GPA</emphasize>{' '}
-                and have completed/am in the process of completing{' '}
-                <emphasize is="custom">34/60</emphasize> of my required credits.
-              </span>
-              <span>
-                <bold is="custom">Berkeley</bold> <br />
-                Berkeley is my "dream school." I wish to attend Berkeley
-                studying computer science. I love the school for many reasons
-                including its amazing (and practical) computer science program,
-                its location in my home state (California), and its proximity to
-                Silicon Valley which may bring many opportunities for
-                internships.
+                <ul className="collapsible">
+                  <li>
+                    <div className="collapsible-header">
+                      <bold is="custom">MBHS</bold>
+                    </div>
+                    <div className="collapsible-body">
+                      I am currently a Junior at Mission Bay High School. I
+                      currently take part in many extracurriculars that I love
+                      doing (most listed to the right). I am a co-founder of the
+                      Mission Bay High School Entrepreneurship Club and
+                      academically, I currently hold a{' '}
+                      <emphasize is="custom">cumulative 4.84 GPA.</emphasize>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="collapsible-header">
+                      <bold is="custom">SDCCD</bold>
+                    </div>{' '}
+                    <div className="collapsible-body">
+                      I am currently a student in the San Diego Community
+                      College District pursuing an{' '}
+                      <emphasize is="custom">
+                        Associate Degree of Science in Computer Science.
+                      </emphasize>{' '}
+                      I currently hold a{' '}
+                      <emphasize is="custom">4.0 GPA</emphasize> and have
+                      completed/am in the process of completing{' '}
+                      <emphasize is="custom">34/60</emphasize> of my required
+                      credits.
+                    </div>
+                  </li>
+                  <li>
+                    <div className="collapsible-header">
+                      <bold is="custom">Berkeley</bold>
+                    </div>
+                    <div className="collapsible-body">
+                      Berkeley is my "dream school." I wish to attend Berkeley
+                      studying computer science. I love the school for many
+                      reasons including its amazing (and practical) computer
+                      science program, its location in my home state
+                      (California), and its proximity to Silicon Valley which
+                      may bring many opportunities for internships.
+                    </div>
+                  </li>
+                </ul>
               </span>
             </ImageSection>
             <ImageSection title="My groups" images={[cc, music, first]}>
               <span>
-                <bold is="custom">Cross Country/Track & Field</bold> <br /> I am
-                currently a member of my school's Track & Field and Cross
-                Country teams. I enjoy running as a sport and to stay healthy!
-              </span>
-              <span>
-                <bold is="custom">Music</bold> <br /> I am currently a violinist
-                in{' '}
-                <a
-                  href="https://missionbaymusic.com/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  my school's symphony orchestra.
-                </a>{' '}
-                Outside of school, I enjoy playing the violin and piano which I
-                have been learning for three and five years respectively. I also
-                played the violin in my district's honor ensemble for one year
-                in middle school and one year in high school so far.
-              </span>
-              <span>
-                <bold is="custom">Robotics</bold> <br /> I am one of the student
-                leaders on{' '}
-                <a
-                  href="https://team4160.com/"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  my school's robotics team.
-                </a>{' '}
-                We compete annually in FRC (First Robotics Competition) and my
-                job is to oversee most of the programming on our team. In the
-                past, I have implemented vision processing code that used
-                cameras and algorithms to calculate and attempt to align our
-                robot properly. In addition to robot related code, I also
-                created our team's daily attendance system using node + Google
-                Sheets API and wrote most of the code for our website.
+                <ul className="collapsible">
+                  <li>
+                    <div className="collapsible-header">
+                      <bold is="custom">Cross Country/Track & Field</bold>
+                    </div>
+                    <div className="collapsible-body">
+                      I am currently a member of my school's Track & Field and
+                      Cross Country teams. I enjoy running as a sport and to
+                      stay healthy!
+                    </div>
+                  </li>
+                  <li>
+                    <div className="collapsible-header">
+                      <bold is="custom">Music</bold>
+                    </div>
+                    <div className="collapsible-body">
+                      I am currently a violinist in{' '}
+                      <a
+                        href="https://missionbaymusic.com/"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        my school's symphony orchestra.
+                      </a>{' '}
+                      Outside of school, I enjoy playing the violin and piano
+                      which I have been learning for three and five years
+                      respectively. I also played the violin in my district's
+                      honor ensemble for one year in middle school and one year
+                      in high school so far.
+                    </div>
+                  </li>
+                  <li>
+                    <div className="collapsible-header">
+                      <bold is="custom">Robotics</bold>
+                    </div>
+                    <div className="collapsible-body">
+                      I am one of the student leaders on{' '}
+                      <a
+                        href="https://team4160.com/"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        my school's robotics team.
+                      </a>{' '}
+                      We compete annually in FRC (First Robotics Competition)
+                      and my job is to oversee most of the programming on our
+                      team. In the past, I have implemented vision processing
+                      code that used cameras and algorithms to calculate and
+                      attempt to align our robot properly. In addition to robot
+                      related code, I also created our team's daily attendance
+                      system using node + Google Sheets API and wrote most of
+                      the code for our website.
+                    </div>
+                  </li>
+                </ul>
               </span>
             </ImageSection>
           </div>
