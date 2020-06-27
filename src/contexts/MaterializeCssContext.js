91a237c55b8ed703d427790e906cf90af97d563a
battletag13 <matthewlinplusprogramming@gmail.com>
2020-06-26 18:51:42 -0700

    Changed MaterializeCssContext to expose M (materialize plugin)

--- src/contexts/MaterializeCssContext.js
+++ src/contexts/MaterializeCssContext.js
@@ -14,6 +14,7 @@ const MaterializeCssContextProvider = (props) => {
     <MaterializeCssContext.Provider
       value={{
         materializeReinit: materializeReinit,
+        M: M,
       }}>
       {props.children}
     </MaterializeCssContext.Provider>
